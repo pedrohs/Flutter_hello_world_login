ecb267556bd56938fea8a95b83b2579818eb0c92
Pedro Henrique <9.pedro.deoliveira@gmail.com>
2020-04-06 14:00:21 -0300

    add rotas e paginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  Widget build(BuildContext context){
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeApp(),
+        '/page2': (context) => Page2()
+      },
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  Widget build(BuildContext context){
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Ola Mundo!"),
+        appBar: AppBar(title: Text("Login"),),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Texto de exemplo"),
+              Text("Texto de exemplo"),
+              Text("Texto de exemplo"),
+              Text("Texto de exemplo")
+            ],
+          )
         ),
-        body: Text("meu primeiro app flutter"),
-        )
-    )
-  );
+          ),
+      )
+    );
+  }
+}
+
+class HomeApp extends StatelessWidget {
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Home Page"),),
+        body: Center(
+          child: RaisedButton(
+            child: Text("segunda pagina"), onPressed: () {
+              Navigator.pushNamed(context, '/page2');
+            },
+            ),
+          ),
+      )
+    );
+  }
 }
